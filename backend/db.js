f2449b3d8445525c32ee1330cbf3fbdc902c3c6b
DogMathism <artyomarutynyan0127@gmail.com>
2025-10-27 14:40:25 +0300

    Create db.js

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+// db.js
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/adaptive'
+});
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  pool
+};
